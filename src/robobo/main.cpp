9ed2ff0c4dda99a74d65b5730484473590275cfb
Leszek Wojcik <agh647@o2.pl>
2018-01-18 21:39:52 +0100

    Added FreeRTOS. Modified Variant hooks to remove loop and setup

--- src/robobo/main.cpp
+++ src/robobo/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include "Robobo.h"
 #include "EncoderISR.h"
+#include "Arduino_FreeRTOS.h"
+#include "task.h"
 
 int led = 13;
 
@@ -11,23 +13,20 @@ int main(void)
     r.createSetupV1();
 	Serial.begin(r.serialSpeed);
 
-	pinMode(led, OUTPUT);     
     
-	for (;;) {
-		digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-		delay(200);               // wait for a secon
-		digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-		delay(50);                 // wait for a second
+    digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(200);               // wait for a secon
+    digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+    delay(50);                 // wait for a second
 
-		r.dc1->setPosition(100);
-        encoder1_ISR();
-		delay(5000);              
-		Serial.println(r.dc1->getPosition());
-        r.dc1->stop();
-        break;
+    r.dc1->setPosition(100);
+    encoder1_ISR();
+    delay(5000);              
+    Serial.println(r.dc1->getPosition());
+    r.dc1->stop();
 
-		if (serialEventRun) serialEventRun();
-	}
+	vTaskStartScheduler(); // initialise and run the freeRTOS scheduler. Execution should never return here.
+	vApplicationMallocFailedHook(); // Probably we've failed trying to initialise heap for the scheduler. Let someone know.
         
 	return 0;
 }
